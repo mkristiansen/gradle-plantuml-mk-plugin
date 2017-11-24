9bbfb0a8a33ee2068f5bc010568e58b8e2520cab
Morten Kristiansen <morten@ie.ibm.com>
2017-11-24 15:01:57 +0000

    Refactors sample plugin project to working plantuml plugin Significant amount of code copied from dirkraft plugin.

--- src/main/groovy/io/kristiansen/gradle/PlantUMLPlugin.groovy
+++ src/main/groovy/io/kristiansen/gradle/PlantUMLPlugin.groovy
@@ -1,23 +1,107 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Jason Dunkelberger (a.k.a dirkraft)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package io.kristiansen.gradle
 
-import io.kristiansen.gradle.greeting.GreetingModule
+import net.sourceforge.plantuml.FileFormat
+import net.sourceforge.plantuml.FileFormatOption
+import net.sourceforge.plantuml.SourceStringReader
+import org.apache.commons.io.FilenameUtils
+import org.gradle.api.DefaultTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.Delete
+import org.gradle.api.tasks.TaskAction
 
-/**
- * The plugin's entry point.
- * Reference:
- * https://docs.gradle.org/3.5/userguide/custom_plugins.html#sec:writing_a_simple_plugin
- * Example 41.1
- */
-public class PlantUMLPlugin implements Plugin<Project>{
+import java.nio.file.Files
+import java.nio.file.Path
 
-    /**
-     * Applies this plugin to the given Gradle project
-     * @param project The Gradle project
-     */
+class PlantUMLPlugin implements Plugin<Project> {
+    @Override
     void apply(Project project) {
-        // Apply all functionality from the GreetingModule
-        GreetingModule.load(project)
+        project.task('renderPlantUml', type: RenderPlantUmlTask)
+        project.task('cleanPlantUml', type: CleanPlantUmlTask)
+    }
+}
+
+class RenderPlantUmlTask extends DefaultTask {
+
+    def Path assetsPath = project.projectDir.toPath().resolve('assets/')
+
+    RenderPlantUmlTask() {
+        for (Path puml : Files.newDirectoryStream(assetsPath, '*.puml')) {
+            inputs.file puml.toFile()
+        }
+
+        for (Path puml : Files.newDirectoryStream(assetsPath, '*.puml')) {
+            outputs.file getDestination(puml.toFile(), '.svg').toFile()
+            outputs.file getDestination(puml.toFile(), '.png').toFile()
+        }
+    }
+
+    Path getDestination(File puml, String extension) {
+        String baseName = FilenameUtils.getBaseName(puml.name)
+        String destName = "${baseName}"
+        assetsPath.resolve(destName + extension)
+    }
+
+
+    @TaskAction
+    def render() {
+
+        Path projectPath = project.projectDir.toPath()
+        for (File puml : inputs.files) {
+            String relPumlPath = projectPath.relativize(puml.toPath()).toString()
+            String pumlContent = new String(Files.readAllBytes(puml.toPath()), 'UTF-8')
+
+            SourceStringReader reader = new SourceStringReader(pumlContent)
+            Path destPathSvg = getDestination(puml, '.svg')
+            println "Rendering ${relPumlPath} to ${projectPath.relativize(destPathSvg)}"
+            reader.generateImage(new FileOutputStream(destPathSvg.toFile()), new FileFormatOption(FileFormat.SVG))
+
+            reader = new SourceStringReader(pumlContent)
+            Path destPathPng = getDestination(puml, '.png')
+            println "Rendering ${relPumlPath} to ${projectPath.relativize(destPathPng)}"
+            reader.generateImage(new FileOutputStream(destPathPng.toFile()), new FileFormatOption(FileFormat.PNG))
+        }
+
+    }
+}
+
+class CleanPlantUmlTask extends Delete {
+
+    def Path assetsPath = project.projectDir.toPath().resolve('assets/')
+
+    CleanPlantUmlTask() {
+        for (Path puml : Files.newDirectoryStream(assetsPath, '*.puml')) {
+            delete getDestination(puml.toFile(), '.svg').toFile()
+            delete getDestination(puml.toFile(), '.png').toFile()
+        }
+    }
+
+    Path getDestination(File puml, String extension) {
+        String baseName = FilenameUtils.getBaseName(puml.name)
+        String destName = "${baseName}"
+        assetsPath.resolve(destName + extension)
     }
 }
--- src/test/groovy/io/kristiansen/gradle/PlantUMLPluginTest.groovy
+++ src/test/groovy/io/kristiansen/gradle/PlantUMLPluginTest.groovy
@@ -1,4 +1,4 @@
-package io.kristiansen.gradle.greeting
+package io.kristiansen.gradle
 
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
@@ -13,17 +13,40 @@ import static org.gradle.testkit.runner.TaskOutcome.*
  * https://docs.gradle.org/3.5/userguide/test_kit.html#sec:functional_testing_with_the_gradle_runner
  * Example 45.3
  */
-class GreetingModuleTest {
+class PlantUMLPluginTest {
     @Rule
     public final TemporaryFolder testProjectDir = new TemporaryFolder();
 
     private File build_gradle
+    private File sample_puml
+    private File sample_svg
+    private File sample_png
 
     @Before
     public void setup() {
         // Prepare build.gradle
+        sample_puml = testProjectDir.newFolder('assets')
+        sample_puml = testProjectDir.newFile('assets/sample.puml')
+        sample_puml << """@startuml
+
+                title narcissism as a stable configuration
+
+                participant enamored as A
+                participant enamored as B
+                participant narcissist as C
+
+                A -> B: likes
+                B --> A: no reciprocation
+                B -> C: likes
+                C --> B: no reciprocation
+                C -> C: likes
+                C -> C: reciprocated
+                @enduml"""
+        sample_svg = testProjectDir.newFile("assets/sample.svg")
+        sample_png = testProjectDir.newFile("assets/sample.png")
+
         build_gradle = testProjectDir.newFile('build.gradle')
-        build_gradle << 'plugins { id "io.kristiansen.gradle.PlantUMLPlugin" }\n'
+        build_gradle << 'plugins { id "io.kristiansen.gradle.PlantUMLPlugin"}\n'
     }
 
     /**
@@ -47,13 +70,34 @@ class GreetingModuleTest {
     }
 
     @Test
-    public void helloWorld_standard() {
-        def result = gradle('helloWorld')
-        assert result.task(":helloWorld").outcome == SUCCESS
-        assert result.output.contains("Hello, world!")
+    public void Tasks() {
+        def result = gradle('tasks')
+        assert result.task(":tasks").outcome == SUCCESS
     }
 
     @Test
+    public void RenderPlantUML() {
+        sample_svg.delete()
+        sample_png.delete()
+
+        def result = gradle('renderPlantUml')
+
+        assert result.task(":renderPlantUml").outcome == SUCCESS
+        assert sample_png.exists() == true
+        assert sample_svg.exists() == true
+    }
+
+    @Test
+    public void CleanPlantUML() {
+        def result1 = gradle('renderPlantUml')
+        def result = gradle('cleanPlantUml')
+        assert result.task(":cleanPlantUml").outcome == SUCCESS
+        assert sample_png.exists() == false
+        assert sample_svg.exists() == false
+    }
+
+
+    /*@Test
     public void helloWorld_alternative() {
         build_gradle << 'greeting.alternativeGreeting = "Howdy"'
 
@@ -107,5 +151,5 @@ class GreetingModuleTest {
 
         assert result.task(":badTask").outcome == FAILED
         assert result.output.contains("I can't let you do that, Starfox.")
-    }
+    }*/
 }
